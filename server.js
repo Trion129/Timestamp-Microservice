bf51874935a9898c0145f90d181caab26a8b1f7a
Parminder Singh <parmsingh129@yahoo.com>
2016-05-06 10:58:45 +0530

    Working now
    Added my functionality

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
 'use strict';
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
+var express = require("express");
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+app.get("/",function(req,res){
+    res.send("<h2>Usage is https://project1-trion129-2.c9users.io/December%2015,%202015<br>or<br>https://project1-trion129-2.c9users.io/1450137600</h2>");
+    res.end();
+})
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.get("/:time",function(req,res){
+    var natural;
+    var date;
+    var unixTime;
+    if(req.params.time.match(/[A-Z]+ [0-9]+\, [0-9]+/gi)){
+        natural = req.params.time;
+        unixTime = Date.parse(req.params.time)/1000;
+    }
+    else if(req.params.time.match(/[0-9]+/gi)){
+        var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+        unixTime = req.params.time;
+        date = new Date(Number(unixTime) * 1000);
+        natural = months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+    }
+    else{
+        res.write("null");
+        res.end();
+    }
+    res.write(JSON.stringify(
+       {
+           unix: unixTime,
+           natural: natural
+        }));
+    res.end();
+});
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
 
 var port = process.env.PORT || 8080;
+
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
